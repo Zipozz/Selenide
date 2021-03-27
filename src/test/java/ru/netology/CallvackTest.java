51615bc19f205f842f80a8a56fec7e622e3e4d9b
Alexsandr Orlov <zipoz1@rambler.ru>
2021-03-27 18:21:38 +0300

    initial commit

---
+++ src/test/java/ru/netology/CallvackTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CallbackTest {
+
+
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Василий");
+        $("[data-test-id=phone] input").setValue("+79876543210");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
+
